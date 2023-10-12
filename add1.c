595d9f165dea742dd1d95f8174e4a4509be630d0
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-10-12 21:57:17 +0000

    week1_notes

---
+++ add1.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main() {
+
+float x = 5;
+float y= 4;
+
+float add= x + y;
+
+printf("add:%f\n", add);
+
+return 0;
+
+}
---
+++ rectanglearea.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main() {
+
+    int width = 0;
+    int length = 0;
+
+    printf("width: ");
+    scanf("%d", &width);
+
+    printf("length: ");
+    scanf("%d", &length);
+
+    int area = width * length;  // Declare and compute the area here.
+
+    printf("Area : %d\n", area);
+
+    return 0;
+}
+
+
+
