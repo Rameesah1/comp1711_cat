bd0228384f10745cdbad1810e9e67c45f9f9240b
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-10-13 13:12:00 +0000

    session 2

---
+++ addtaskalt.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+int main (){
+
+    float x= 5.7;
+    float y = 4.5;
+    float z = x + y;
+
+
+   printf("The sum of adding %f and %f is %f", x , y , z);
+
+   return 0;
+
+}
