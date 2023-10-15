677b6d8c2c2be283148743a370aaf4e65f89c5ff
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-10-15 17:59:43 +0000

    session2work

---
+++ week1_c_bootcamp1/array1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+  int multipleVariables[]= {55,65,64,76}; //this assigns a variable with multiple values (array)
+  printf("%d", multipleVariables[1]);
+
+return 0;
+}
