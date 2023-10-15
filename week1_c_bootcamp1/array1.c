62e57d2a4fda68442f8a286d2f5a6899ad269c60
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-10-15 18:04:59 +0000

    session2work

--- week1_c_bootcamp1/array1.c
+++ week1_c_bootcamp1/array1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 int main() {
   int multipleVariables[]= {55,65,64,76}; //this assigns a variable with multiple values (array)
-  printf("%d", multipleVariables[1]);
+  multipleVariables[0]=34;  //NOT NECCESSARY- this just changes the value of the first element
+
+  printf("%d", multipleVariables[0]); //prints the new value - 34 instead of 55
 
 return 0;
 }
