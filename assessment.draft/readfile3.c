55c8bd013bd025cc524927ff268983a8c5c96120
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-10-31 20:51:54 +0000

    Submit

--- assessment.draft/readfile3.c
+++ assessment.draft/readfile3.c
@@ -39,35 +39,35 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 int main() {
     char filename[] = "FitnessData_2023.csv";
-    FILE *file = fopen(filename, "r"); // "r" means read mode
+    FILE *file = fopen(filename, "r"); // r to read the file - not w as there is no writing in
     if (file == NULL) {
         printf("Error in opening file");
         return 1;
     }
 
-    FITNESS_DATA Fitnessdata[60]; // Can store up to 61 records from the file
+    FITNESS_DATA Fitnessdata[61]; // Can store up to 60 records from the file
     int records = 0;
-    // Read each line of the file and store each record into the Fitness_Data array
-    for (records = 0;  records < 61 && fscanf(file, "%11[^,],%6[^,],%d\n", 
+
+    for (records = 0;  records < 60 && fscanf(file, "%11[^,],%6[^,],%d\n",   //// This reads each line of the file and stores each record into the Fitness_Data array
                                  Fitnessdata[records].date, 
                                  Fitnessdata[records].time, 
-                                 &Fitnessdata[records].steps) == 3; records++) {
+                                 &Fitnessdata[records].steps) == 3; records++) {  //& used for steps as this is an integer
       
     }
 
-    fclose(file); // Always close the file handle once you're done with it.
+    fclose(file); //file closed 
 
     // Check if there are at least three records to print
-    if (records < 3) {
+    if (records < 3) {                                     //checks if there are three records to print at least
         printf("Not enough records in the file.\n");
         return 1;  // exit with an error code
     }
-
+    else {
     // Print the number of records in the required format
     printf("Number of records in file: %d\n", records);
-
-    // Print the first three records in the desired format
-    int storage = 0 //or you can skip this step and assign the int storage to zero in the for loop
+      }
+    // Prints the first three records 
+    int storage; //or you can skip this step and assign the int storage to zero in the for loop. You can use i 
     for (storage = 0; storage < 3; storage++) {  // Only the first three records
         printf("%s/%s/%d\n", 
                Fitnessdata[storage].date, 
