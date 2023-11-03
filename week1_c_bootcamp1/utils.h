398bdc8e79b5d1a00e814008a6a8e05e7c2725b2
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-11-03 01:32:52 +0000

    read

--- week1_c_bootcamp1/utils.h
+++ week1_c_bootcamp1/utils.h
@@ -1,13 +1,33 @@
-
+// Include guards - prevents multiple includes which cause errors!
 #ifndef UTILS_H
 #define UTILS_H
-#include <stdlib.h>
+
+// These will be included in any file which includes this header file!
 #include <stdio.h>
+#include <stdlib.h>
 
-FILE *open_file(char* filename, char* mode)
+FILE *open_file(char *filename, char *mode);
 
-#endif /* UTILS_H */
+// this is a doxygen comment - you'll see more of these in COMP1921 :)
+/**
+ * @brief Opens the specified file
+ *
+ * @param filename the path to the file to be opened.
+ * @param mode the mode ( r/w/a | + )
+ * @return the opened file pointer
+ */
+FILE *open_file(char *filename, char *mode)
+{
+    FILE *file = fopen(filename, mode);
 
-#inclide "utils.h"
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        exit(1);
+    }
 
-File *open file(char filename[])
+    return file;
+}
+
+// this is closing the include guards.
+#endif
