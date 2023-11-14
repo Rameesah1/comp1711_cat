5b647d2891c78973cd559da2187512dee3281d8a
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-11-14 13:20:39 +0000

    starter

--- week1_c_bootcamp1/structrand.c
+++ week1_c_bootcamp1/structrand.c
@@ -1,4 +1,5 @@
-#include <stdio>
+#include <stdio.h>
+
    typedef struct
 {
 char date[20];
@@ -9,17 +10,22 @@ float bloodIron;
 int main () {
     //creating a variable in the struct 'reading'
     reading today;  
-    reading1;
-    reading2;
-    reading3;
-    reading4;
-    reading5;
-    reading6;
-    reading7;
+    reading reading1;
+    reading reading2;
+    reading reading3;
+    reading reading4;
+    reading reading5;
+    reading reading6;
+    reading reading7;
     //array of structs 
     reading thisWeek[7]= {reading1, reading2, reading3, reading4, reading5, reading6, reading7};
 
+    for (int i=0; i<7; i++)
+    {
+        thisWeek[i].date[0] = '\0';
+        thisWeek[i].bloodIron=0;
+    }
 
-
+    return 0;
 
 }
