b6a9c07a1dd15dd902176359f1babea50a84384c
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-11-17 19:24:46 +0000

    finish A

--- assessment.draft/StepCounter_Final.c
+++ assessment.draft/StepCounter_Final.c
@@ -39,9 +39,9 @@ void tokeniseRecord(const char *input, const char *delimiter,
 
 // Complete the main function
 int main() {
-     int choice, i;
+     char choice;
 
-while ()
+while (1)
 {   
     printf("A. Specify the filename to be imported – you need to check that the file opened correctly\n");
     printf("B. Display the total number of records in the file\n"); 
@@ -49,50 +49,60 @@ while ()
     printf("D. Find the date and time of the timeslot with the most steps\n"); 
     printf("E. Find the data and time of the timeslot with the largest number of steps\n");
     printf("F. Find the longest continuous period where the step count is above 500 steps\n");
-    printf("Q. Exit");
-    printf("Enter choice: "\n);
-    scanf("%c", &choice \n);
+    printf("Q. Exit the menu\n");
+    printf("Enter choice:\n ");
+    scanf("%c", &choice);
+    while (getchar() != '\n') //idea from stack overflow- clears the input buffer. The while loop has a function inside it (getchar) and thsi reads each character of teh standard input. The while loop only gets executed if the character is not equal to != '\n' (so the newline character when the user adds enter).
   
   switch(choice)  {
     
-    case A:
+    case 'A':  {
     
-    char filename[] = "FitnessData_2023.csv";
+    char filename[100];
+    printf("Enter file name:  \n");
+    scanf("%99s", filename);  //not &filename as its not an int. use array name - filename- only
+
     FILE *file = fopen(filename, "r"); // r to read the file - not w as there is no writing in
     if (file == NULL) {
         printf("Error in opening file");
         return 1;
+    } else {
+        printf("File opened successfully\n");
+        fclose(file); // Close the file
+        } break;
     }
-    
 
-    case B:
+    case 'B':
     printf("24/05/2023 - 13:00"); break;
 
-    case C:
+    case 'C':
     printf("21/05/2023 - 14:00"); break;
 
-    case D:
+    case 'D':
     printf("21/05/2023 - 14:00"); break;
 
-    case E:
+    case 'E':
     printf("21/05/2023 - 14:00"); break;
 
-    case F:
+    case 'F':
     printf("21/05/2023 - 14:00"); break;
 
-    case Q :
-    printf("21/05/2023 - 14:00"); break;
+    case 'Q' :
+    printf("Exiting");
+    return 0;
+    break; 
 
 
     
 
   }
-  return 0;
+  
         
 
 
 
-} 
+}
+return 0; 
 }
 
 
