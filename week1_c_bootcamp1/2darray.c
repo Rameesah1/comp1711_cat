01c90aec2f525dd42ad2124a434cc8dae1ffe48a
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-12-02 16:09:12 +0000

    continue c

--- week1_c_bootcamp1/2darray.c
+++ week1_c_bootcamp1/2darray.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void printArray(int a[][3] ){
-for (size_t i = 0; i <= 1; ++i) {
-for (size_t j = 0; j <= 2; ++j) {
-printf("%d ", a[i][j]);
+for (int i = 0; i <= 1; ++i) {  //better to use size_t as this is better for arrays
+for (int j = 0; j <= 2; ++j) {
+printf("%d ", a[i][j]); //must use whitespace after format specifier so there is a space between the elements 
 }
 printf("\n"); // start new line of output
 }
 }
 int main() {
-int array1[2][3] = {{1, 2, 3}, {4, 5, 6}};
+int array1[2][3] = {{1, 2, 3}, {4, 5, 6}};  //best way to initialise
 printf("Values in array1 by row are:\n");
 printArray(array1);
 int array2[2][3] = {1, 2, 3, 4, 5};
