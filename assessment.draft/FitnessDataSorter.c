bb2205cbaf948e3edd6ddd8d386f5c045fe33e7d
Rameesah1 <146842612+Rameesah1@users.noreply.github.com>
2023-12-10 21:17:38 +0000

    continue c

--- assessment.draft/FitnessDataSorter.c
+++ assessment.draft/FitnessDataSorter.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+
 // Define the struct for the fitness data
 typedef struct {
     char date[11];
     char time[6];
     int steps;
 } Fitnessdata;
+
+
 
 
 
@@ -27,70 +30,43 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
     }
 }
 
+    int import_file(char *filename, FITNESS_DATA Fitnessdata[], int *records) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Could not find or open the file.\n");
+        return 0;
+    }
+
+    char line_buffer[100];
+    char date[11], time[6], steps[10];
+    *records = 0;
+
+    while (*records < 1000 && fgets(line_buffer, sizeof(line_buffer), file) != NULL) {
+        tokeniseRecord(line_buffer, "," , date, time, steps);
+
+        strcpy(Fitnessdata[*records].date, date);
+        strcpy(Fitnessdata[*records].time, time);
+        Fitnessdata[*records].steps = atoi(steps);  
+        
+        (*records)++;
+    }
+
+    fclose(file);
+    return 1;
+}
+
+
+
 
 int main() {
 
-    char choice;
-    char actualfilename[21]= "FitnessData_2023.csv";
-    int value;  //will be used for strcmp function to check if the value is the same. 
-    
-                                   // Global declaration of the Fitnessdata array
-    int records = 0;
-   
 
-    for (filename == "FitnessData_2023.csv");
-    while(1) {
-    char filename[20];
-                printf("Enter file name:\n");
-                scanf(" %20s", filename); // Use a space before %99s to ignore any leading whitespace
-                
-                value = strcmp(filename, actualfilename) ;  //first filename is the one user will enter/ second one is the actual one
-                if (value == 0) {
-    
-                FILE *file = fopen(actualfilename, "r"); // Try to open the file for reading
-                if (file == NULL) {
-                    printf("Error opening file"); // Use  to print the error message
-                } else {
-                    printf("File opened successfully\n");
-                    fclose(file); // Close the file if it's opened successfully
-                
-                }
-                }   else { 
-                    printf("Incorrect file name entered. Please try again\n");
 
-     
-               }
-
-    
-    }
-               return 0;
-    }
-
-    /*}
-    //to make the file in descending order of step count
-
-       for (int i = 0; i < records; i++)
-{
-    for (int j = 0; j < (records - 1); j++)
-    {
-        if (Fitnessdata[j].steps > Fitnessdata[j+1].steps)
-        {
-            int temp = Fitnessdata[j].steps;
-            Fitnessdata[j].steps = Fitnessdata[j+1].steps;
-            Fitnessdata[j + 1].steps = temp;
+        printf("Input filename:\n ");
+        scanf("%s", filename); 
+        if (!import_file(filename, Fitnessdata, &records)) {
+        return 1;
         }
-    }
+
+        return 0;
 }
-
-for (int i = 0; i < records; i++)
-{
-    printf("a[%d] = %d\n", i, Fitnessdata[i].steps);
-}
-
-      
-
-
-
-return 0;
-}
-*/
